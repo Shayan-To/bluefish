41cb6ad914263a60ce1625820a552421398f4b5d
dleidert <dleidert@7640d186-872a-4c71-b7b2-a5696852cc35>
2008-11-01 17:43:33 +0000

    clean-up: Remove /bluefish/ from tags/branches.

--- testcode/bftestview2/bftextview2_autocomp.h
+++
@@ -1,8 +0,0 @@
-#ifndef _BFTEXTVIEW2_AUTOCOMP_H_
-#define _BFTEXTVIEW2_AUTOCOMP_H_
-
-#include "bftextview2.h"
-gboolean acwin_check_keypress(BluefishTextView *btv, GdkEventKey *event);
-void autocomp_run(BluefishTextView *btv, gboolean user_requested);
-
-#endif /* _BFTEXTVIEW2_AUTOCOMP_H_ */
